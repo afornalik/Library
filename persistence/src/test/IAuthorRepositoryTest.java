83d065bb589e31908caed4efbc598c8718050dfe
Andrzejf <afornalik2@gmail.com>
2019-07-12 00:01:28 +0200

    Create test for Author repository

--- persistence/src/test/IAuthorRepositoryTest.java
+++ persistence/src/test/IAuthorRepositoryTest.java
@@ -1,5 +1,8 @@
 import model.Author;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import repository.AuthorRepository;
 import repository.IAuthorRepository;
 import utils.EntityManagerUtils;
 
@@ -7,28 +10,77 @@ import javax.persistence.EntityManager;
 
 public class IAuthorRepositoryTest {
 
-    private IAuthorRepository IAuthorRepository;
-    private EntityManagerUtils entityManagerUtils = EntityManagerUtils.getInstance();
+    private IAuthorRepository iAuthorRepository;
 
-    @Test
-    public void ShouldAddNewAuthor() {
-        EntityManager entityManager = entityManagerUtils.getEntityManager();
-
-        System.out.println(entityManager.find(Author.class,1l));
-
-
+    @Before
+    public void init() {
+        iAuthorRepository = new AuthorRepository();
     }
 
     @Test
-    public void ShouldAddNew() {
-        EntityManager entityManager = entityManagerUtils.getEntityManager();
+    public void shouldReturnFirstAuthor(){
+        //given
+
+
+        //when
+        Author result = iAuthorRepository.getAuthorById(1L);
+
+        //then
+        Assert.assertEquals(result.getFirstName(),"Jan");
+        Assert.assertEquals(result.getLastName(),"Brzechwa");
+    }
+
+    @Test
+    public void shouldReturnNumbersOfAuthors() {
+        //given
+
+        //when
+        int result = iAuthorRepository.getAllAuthors().size();
+
+        //then
+        Assert.assertEquals(result,4);
+    }
+
+    @Test
+    public void shouldSaveAuthor() {
+        //given
         Author author = new Author();
+        author.setFirstName("Herbert");
+        author.setLastName("Shildt");
 
-        author.setBirthPlace("d");
-        author.setFirstName("Adam");
-        author.setLastName("Mickiewicz");
-        entityManager.getTransaction().begin();
-      //  entityManager.persist(author);
-        entityManager.getTransaction().commit();
+        //when
+        iAuthorRepository.save(author);
+
+        //then
+        Assert.assertEquals(iAuthorRepository.getAllAuthors().size(),5);
+        iAuthorRepository.delete(author);
     }
+
+    @Test
+    public void shouldEditAuthor() {
+        //given
+        Author author = iAuthorRepository.getAuthorById(4L);
+        author.setFirstName("Adam");
+
+        //when
+        iAuthorRepository.edit(author);
+
+        //then
+        Assert.assertEquals(iAuthorRepository.getAuthorById(4L).getFirstName(),"Adam");
+    }
+
+    @Test
+    public void shouldDeleteAuthor() {
+        //given
+        Author author = iAuthorRepository.getAuthorById(6L);
+
+        //when
+        iAuthorRepository.delete(author);
+
+        //then
+        Assert.assertNull(iAuthorRepository.getAuthorById(6L));
+    }
+
+
+
 }
